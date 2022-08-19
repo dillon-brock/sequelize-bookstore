be4044a252aca6be06ed0e0a9eea0f846f4b04e0
Dillon Brock <mdillon1297@gmail.com>
2022-08-18 21:01:03 -0700

    added genre table with one to many relationship with books, modified existing tests to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,12 +4,47 @@ const db = require('../lib/models');
 
 describe('backend-express-template routes', () => {
   beforeEach(async () => {
-    // await db.sequelize.sync({ force: true });
     try {
       const queryInterface = db.sequelize.getQueryInterface();
       await queryInterface.bulkDelete('BookAuthors', null, {});
+      await queryInterface.bulkDelete('Genres', null, {});
       await queryInterface.bulkDelete('Books', null, {});
       await queryInterface.bulkDelete('Authors', null, {});
+      const genres = await db.Genre.bulkCreate([
+        {
+          name: 'Fiction', // id: 1
+        },
+        {
+          name: 'Historical Fiction', // id: 2
+        },
+        {
+          name: 'Non-Fiction', // id: 3
+        },
+        {
+          name: 'Biography', // id: 4
+        },
+        {
+          name: 'Memoir', // id: 5
+        },
+        {
+          name: 'Poetry', // id: 6
+        },
+        {
+          name: 'Short Stories', // id: 7
+        },
+        {
+          name: 'Science Fiction', // id: 8
+        },
+        {
+          name: 'Fantasy', // id: 9
+        },
+        {
+          name: 'Cookbook', // id: 10
+        },
+        {
+          name: 'Reference', // id: 11
+        },
+      ]);
       const authors = await db.Author.bulkCreate([
         {
           firstName: 'Neil',
@@ -41,43 +76,180 @@ describe('backend-express-template routes', () => {
           dob: new Date('1974-02-15'),
           pob: 'Barre, VT',
         },
+        {
+          firstName: 'Chimamanda Ngozi',
+          lastName: 'Adichie',
+          dob: new Date('1977-09-15'),
+          pob: 'Enugu, Nigeria',
+        },
+        {
+          firstName: 'Donna',
+          lastName: 'Tartt',
+          dob: new Date('1963-12-23'),
+          pob: 'Greenwood, MS',
+        },
+        {
+          firstName: 'Anthony',
+          lastName: 'Doerr',
+          dob: new Date('1973-10-27'),
+          pob: 'Cleveland, OH',
+        },
+        {
+          firstName: 'Markus',
+          lastName: 'Zusak',
+          dob: new Date('1975-06-23'),
+          pob: 'Sydney, Australia',
+        },
+        {
+          firstName: 'Frans',
+          lastName: 'Pretorious',
+          dob: new Date('1973-07-31'),
+          pob: 'Johannesburg, South Africa',
+        },
+        {
+          firstName: 'Steven',
+          lastName: 'Gubser',
+          dob: new Date('1972-05-04'),
+          pob: 'Tulsa, OK',
+        },
+        {
+          firstName: 'Doris',
+          lastName: 'Kearns Goodwin',
+          dob: new Date('1943-01-04'),
+          pob: 'New York City, NY',
+        },
+        {
+          firstName: 'Harold',
+          lastName: 'Pender',
+          dob: new Date('1879-01-13'),
+          pob: 'Tarboro, NC',
+        },
+        {
+          firstName: 'Knox',
+          lastName: 'McIlwain',
+          dob: null,
+          pob: null,
+        },
+        {
+          firstName: 'Sylvia',
+          lastName: 'Plath',
+          dob: new Date('1932-10-27'),
+          pob: 'Boston, MA',
+        },
+        {
+          firstName: 'David',
+          lastName: 'Chang',
+          dob: new Date('1977-08-05'),
+          pob: 'Arlington County, VA',
+        },
+        {
+          firstName: 'Peter',
+          lastName: 'Meehan',
+          dob: null,
+          pob: null,
+        },
       ]);
       const books = await db.Book.bulkCreate([
         {
           title: 'American Gods',
           releaseYear: 2001,
+          GenreId: genres[8].id,
         },
         {
           title: 'The Graveyard Book',
           releaseYear: 2008,
+          GenreId: genres[8].id,
         },
         {
           title: 'Crying in H Mart',
           releaseYear: 2021,
+          GenreId: genres[4].id,
         },
         {
           title: 'Educated',
           releaseYear: 2018,
+          GenreId: genres[4].id,
         },
         {
           title: 'Kindred',
           releaseYear: 1979,
+          GenreId: genres[8].id,
         },
         {
           title: 'Parable of the Sower',
           releaseYear: 1993,
+          GenreId: genres[7].id,
         },
         {
           title: 'Xenogenesis',
           releaseYear: 1987,
+          GenreId: genres[7].id,
         },
         {
           title: 'It Chooses You',
           releaseYear: 2011,
+          GenreId: genres[2].id,
         },
         {
           title: 'The First Bad Man',
           releaseYear: 2015,
+          GenreId: genres[0].id,
+        },
+        {
+          title: 'The Thing Around Your Neck',
+          releaseYear: 2009,
+          GenreId: genres[6].id,
+        },
+        {
+          title: 'The Goldfinch',
+          releaseYear: 2013,
+          GenreId: genres[0].id,
+        },
+        {
+          title: 'All The Light We Cannot See',
+          releaseYear: 2014,
+          GenreId: genres[1].id,
+        },
+        {
+          title: 'About Grace',
+          releaseYear: 2004,
+          GenreId: genres[0].id,
+        },
+        {
+          title: 'The Book Thief',
+          releaseYear: 2005,
+          GenreId: genres[1].id,
+        },
+        {
+          title: 'We Should All Be Feminists',
+          releaseYear: 2014,
+          GenreId: genres[2].id,
+        },
+        {
+          title: 'The Little Book of Black Holes',
+          releaseYear: 2017,
+          GenreId: genres[2].id,
+        },
+        {
+          title: 'The Bully Pulpit',
+          releaseYear: 2013,
+          GenreId: genres[3].id,
+        },
+        {
+          // eslint-disable-next-line quotes
+          title: "Electrical Engineers' HandBook",
+          releaseYear: 1914,
+          GenreId: genres[10].id,
+        },
+        {
+          title: 'The Bell Jar',
+          releaseYear: 1963,
+          GenreId: genres[5].id,
+        },
+        {
+          title: 'Momofuku',
+          releaseYear: 2009,
+          GenreId: genres[9].id,
         },
       ]);
       await db.BookAuthor.bulkCreate([
@@ -117,6 +289,62 @@ describe('backend-express-template routes', () => {
           BookId: books[8].id,
           AuthorId: authors[4].id,
         },
+        {
+          BookId: books[9].id,
+          AuthorId: authors[5].id,
+        },
+        {
+          BookId: books[10].id,
+          AuthorId: authors[6].id,
+        },
+        {
+          BookId: books[11].id,
+          AuthorId: authors[7].id,
+        },
+        {
+          BookId: books[12].id,
+          AuthorId: authors[7].id,
+        },
+        {
+          BookId: books[13].id,
+          AuthorId: authors[8].id,
+        },
+        {
+          BookId: books[14].id,
+          AuthorId: authors[5].id,
+        },
+        {
+          BookId: books[15].id,
+          AuthorId: authors[9].id,
+        },
+        {
+          BookId: books[15].id,
+          AuthorId: authors[10].id,
+        },
+        {
+          BookId: books[16].id,
+          AuthorId: authors[11].id,
+        },
+        {
+          BookId: books[17].id,
+          AuthorId: authors[12].id,
+        },
+        {
+          BookId: books[17].id,
+          AuthorId: authors[13].id,
+        },
+        {
+          BookId: books[18].id,
+          AuthorId: authors[14].id,
+        },
+        {
+          BookId: books[19].id,
+          AuthorId: authors[15].id,
+        },
+        {
+          BookId: books[19].id,
+          AuthorId: authors[16].id,
+        },
       ]);
     } catch (e) {
       // eslint-disable-next-line no-console
@@ -131,6 +359,7 @@ describe('backend-express-template routes', () => {
     expect(res.status).toBe(200);
     expect(res.body[0]).toEqual({
       id: expect.any(Number),
+      GenreId: expect.any(Number),
       title: expect.any(String),
       releaseYear: expect.any(Number),
     });
@@ -142,6 +371,7 @@ describe('backend-express-template routes', () => {
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
       id: initialBookId,
+      GenreId: expect.any(Number),
       title: 'American Gods',
       releaseYear: 2001,
       Authors: expect.any(Array),
@@ -190,9 +420,11 @@ describe('backend-express-template routes', () => {
     });
   });
   it('#POST /api/v1/authors/:id/books should add a new book that corresponds to author id from params', async () => {
+    const genres = await db.Genre.findAll();
     const newBook = {
       title: 'Dawn',
       releaseYear: 1987,
+      GenreId: genres[7].id,
     };
     const res = await request(app)
       .post('/api/v1/authors/4/books')
@@ -200,6 +432,7 @@ describe('backend-express-template routes', () => {
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
       id: expect.any(Number),
+      GenreId: expect.any(Number),
       title: 'Dawn',
       releaseYear: 1987,
     });
